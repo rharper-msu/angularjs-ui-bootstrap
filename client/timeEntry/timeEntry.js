d461d348762ecca998399d2afe085e1565b4872f
Richard Harper <rharpe10@msudenver.edu>
2017-04-23 07:36:27 -0600

    Work on date validation

--- client/timeEntry/timeEntry.js
+++ client/timeEntry/timeEntry.js
@@ -6,15 +6,27 @@
                 opened: false
             };
 
+            $scope.formats = ['MM/dd/yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+            $scope.format = $scope.formats[0];
+            $scope.altInputFormats = ['M!/d!/yyyy'];
+
             $scope.dateOptions = {
                 formatYear: 'yyyy',
                 maxDate: new Date(),
-                minDate: new Date(2000, 1, 1),
-                startingDay: 1
+                minDate: new Date(2000, 0, 1),
+                startingDay: 0
             };
 
             $scope.openStartTime = function () {
                 $scope.StartTime.opened = true;
+            };
+
+            $scope.submitForm = function () {
+                if ($scope.startdate) {
+                    console.log('Start date: ' + $scope.startdate);
+                } else{
+                    console.log('Invalid start date: ' + $scope.startdate);
+                }
             };
         });
 })();
