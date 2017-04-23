353e0969fbf73a7b80096e49ce36ec4b8a8e0950
Richard Harper <rharpe10@msudenver.edu>
2017-04-23 06:42:03 -0600

    Work on time entry module

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -1,1 +1,24 @@
-angular.module('timeKeeper', ['ui.bootstrap']);
+'use strict';
+angular.module('timeKeeper', ['ngRoute', 'ui.bootstrap'])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'timeEntry/timeEntry.html',
+                controller: 'timeEntry',
+                resolve: {
+                    // I will cause a delay
+                    delay: function ($q, $timeout) {
+                        var delay = $q.defer();
+                        $timeout(delay.resolve, 200);
+                        return delay.promise;
+                    }
+                }
+            });
+
+        // configure html5 to remove #! routing
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+    });
--- client/timeEntry/timeEntry.js
+++ client/timeEntry/timeEntry.js
@@ -1,3 +1,20 @@
-(function(){
-    
+(function () {
+    angular.module('timeKeeper')
+        .controller('timeEntry', function ($scope, $http) {
+
+            $scope.StartTime = {
+                opened: false
+            };
+
+            $scope.dateOptions = {
+                formatYear: 'yyyy',
+                maxDate: new Date(),
+                minDate: new Date(2000, 1, 1),
+                startingDay: 1
+            };
+
+            $scope.openStartTime = function () {
+                $scope.StartTime.opened = true;
+            };
+        });
 })();
